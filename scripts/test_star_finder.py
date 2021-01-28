8c6863c53947a8973f67c15144bbc41d93562032
Frank Zhao <frank.zhao.main@gmail.com>
2021-01-27 21:17:32 -0800

    Create test_star_finder.py

---
+++ scripts/test_star_finder.py
@@ -0,0 +1,81 @@
+import micropython
+import pyb
+import math
+import star_finder, astro_sensor
+
+cam = astro_sensor.AstroCam(simulate = "polaris.bmp")
+
+print("cam init...", end="")
+cam.init(gain_db = 32, shutter_us = 1500 * 1000)
+
+while cam.check_init() == False:
+    print(".", end="")
+print(" done")
+
+print("capturing...")
+img = cam.snapshot()
+print("done")
+hist = img.get_histogram()
+stats = hist.get_statistics()
+
+stars, code = star_finder.find_stars(img, hist = hist, stats = stats, thresh = (16 * 3), guider = True)
+
+print("stars: %u , mean: %u" % (len(stars), stats.mean()))
+
+bar_len = 10
+
+for i in stars:
+    i.eval()
+    r = i.r()
+    cx = i.cxf()
+    cy = i.cyf()
+    cx_i = int(round(cx))
+    cy_i = int(round(cy))
+    cx_floor = int(math.floor(cx))
+    cy_floor = int(math.floor(cy))
+    cx_ceil  = int( math.ceil(cx))
+    cy_ceil  = int( math.ceil(cy))
+    shade_left   = (cx_ceil - cx) * 255
+    shade_right  = 255 - shade_left
+    shade_top    = (cy_ceil - cy) * 255
+    shade_bottom = 255 - shade_top
+    shade_left   = int(round(shade_left))
+    shade_right  = int(round(shade_right))
+    shade_top    = int(round(shade_top))
+    shade_bottom = int(round(shade_bottom))
+    if cx_floor == cx_ceil:
+        shade_left   = 255
+        shade_right  = 255
+    if cy_floor == cy_ceil:
+        shade_top    = 255
+        shade_bottom = 255
+    img.draw_line(cx_i - r - bar_len, cy_floor, cx_i - r, cy_floor, shade_top)
+    img.draw_line(cx_i + r + bar_len, cy_floor, cx_i + r, cy_floor, shade_top)
+    img.draw_line(cx_i - r - bar_len, cy_ceil , cx_i - r, cy_ceil , shade_bottom)
+    img.draw_line(cx_i + r + bar_len, cy_ceil , cx_i + r, cy_ceil , shade_bottom)
+    img.draw_line(cx_floor, cy_i - r - bar_len, cx_floor, cy_i - r, shade_left)
+    img.draw_line(cx_floor, cy_i + r + bar_len, cx_floor, cy_i + r, shade_left)
+    img.draw_line(cx_ceil , cy_i - r - bar_len, cx_ceil , cy_i - r, shade_right)
+    img.draw_line(cx_ceil , cy_i + r + bar_len, cx_ceil , cy_i + r, shade_right)
+    text_x = 0
+    text_y = 0
+    profile = i.star_profile()
+    if len(profile) > 5:
+        profile = profile[0:5]
+    profile_text = ""
+    for j in profile:
+        profile_text += "%u," % j
+    text = "%u-[%s]" % (i.star_rating(), profile_text)
+    if cx_i >= img.width() // 2:
+        text_x = cx_i - r - bar_len - (len(text) * 9) - 2
+    else:
+        text_x = cx_i + r + bar_len + 2
+    if cy_i < img.height() // 2:
+        text_y = cy_i + r + bar_len + 5
+    else:
+        text_y = cy_i - r - bar_len - 5
+    img.draw_string(text_x, text_y, text, 255)
+
+fname = "test_%u.jpg" % (pyb.rng() % 1000)
+print("saving: " + fname)
+img.save(fname, quality = 100)
